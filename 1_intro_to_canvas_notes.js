f741f3cf00de212c6d08b7736f646b833d4069a6
chrikulhan <98656265+chrikulhan@users.noreply.github.com>
2022-10-10 18:08:47 -0500

    started topic 6, video 1: canvas. Need a dinner break.

---
+++ 1_intro_to_canvas_notes.js
@@ -0,0 +1,25 @@
+// HTML & JavaScript Canvas
+// Maps and Charts
+
+// code examples: https://github.com/claraj/week5-examples
+
+/*Drawing with HTML5 Canvas & JavaScript
+Maps with Leaflet.js
+    Charts with Chart.js
+
+    Optional:
+        Carousels with Flickity.js
+    Code highlighting with Highlight.js
+*/
+
+//Canvas:
+/*
+-Built into HTML5
+-Drawing, animation, controlled by JavaScript
+-Used by online games, graphics, charts, visualizations
+-A <canvas> element is used to contain graphics
+-You'll use JavaScript to draw the graphics - shapes, images, text, gradients…
+-You can move things and redraw and update your canvas as you need.
+
+http://www.w3schools.com/graphics/canvas_intro.asp
+*/
