f3d01855888509544449882f705ab39add9d592c
chrikulhan <98656265+chrikulhan@users.noreply.github.com>
2022-10-18 15:24:37 -0500

    figured out how to add the color selector and pen size selector. Need to figure out how to attach the color selector to the drawing action *doesn't currently work*
    can I make the selector for pen size start at 1?

---
+++ week_6_lab/lab_topic_6_part_1.js
@@ -0,0 +1,11 @@
+// The below will allow for input to select colors:
+
+let colorSelectedInput = document.querySelector('#color')
+let radiusSelectedInput = document.querySelector('#range')
+
+
+
+
+let colorSelected = colorSelectedInput.value
+let radiusSelected = radiusSelectedInput.value
+
